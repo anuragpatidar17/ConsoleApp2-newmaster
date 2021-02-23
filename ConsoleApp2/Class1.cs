a00abc7f5fc012c0282d7cc7c7a8b68b1b8e3adb
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-02-23 15:37:21 +0530

    mds website

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace ConsoleApp2
+{
+
+    public class Class1
+    {
+        [Test]
+        public void SearchForWord()
+        {
+            var driver = new ChromeDriver();
+
+            {
+
+                driver.Navigate().GoToUrl("https://mds-test.shell.com/");
+
+
+                
+
+
+                Console.WriteLine(driver.Title);
+
+
+                
+
+
+
+                driver.Quit();
+            }
+
+        }
+    }
+}
